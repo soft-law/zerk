882c64d645b044784108cf72a6340c9e0a8bb385
Mario <69741055+wolffhack@users.noreply.github.com>
2023-12-20 21:09:40 -0600

    prod

--- front-end/components/Dashboard/Donator.jsx
+++ front-end/components/Dashboard/Donator.jsx
@@ -1,6 +1,7 @@
 import CreateLawyer from "../comps/createLawyer";
 import { Flex, Heading, Text, Box } from "@chakra-ui/react";
 import DonateToCase from "../comps/donate";
+import ValidateLawyer from "../comps/validateLawyer";
 
 export default function LegalVerificator() {
   return (
@@ -8,6 +9,7 @@ export default function LegalVerificator() {
       <Heading>Donate Your $RTM</Heading>
       <Heading>Be a Justice Donator</Heading>
       <DonateToCase />
+      <ValidateLawyer />
     </Box>
   );
 }
