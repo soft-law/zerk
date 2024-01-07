20dc834489f76808447d636a9c25e489008471c2
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-01-07 10:19:38 -0600

    dashBoard design update

--- front-end/components/Dashboard/index.jsx
+++ front-end/components/Dashboard/index.jsx
@@ -5,39 +5,56 @@ import {
   TabPanels,
   TabPanel,
   Flex,
+  Image,
 } from "@chakra-ui/react";
 import HeadDash from "../comps/HeadDash";
 import LegalVerificator from "./Lawyer";
 import Juster from "./Juster";
 import Donator from "./Donator";
 import Admin from "./Admin";
+import { color } from "framer-motion";
 export default function Dashboard() {
   const styleTabList = {
-    mt: "7rem",
-    mb: "7rem",
+    mt: "1rem",
+    mb: "1rem",
     ml: "1rem",
     mr: "1rem",
     fontSize: "xl",
+    color: "gray",
+    _hover: { color: "#adff00" },
   };
 
   return (
     <>
       <Tabs
         isManual
-        variant="none"
+        variant="lazy"
         orientation="vertical"
         isFitted
         bgImage="section2.svg"
         bgSize="cover"
         bgRepeat="no-repeat"
       >
-        <TabList>
-          <Flex bgColor="Black" direction="column">
+        <TabList h="100vh">
+          <Flex bgColor="Black" direction="column" h="100vh" justify="left">
             <HeadDash sx={styleTabList} />
-            <Tab sx={styleTabList}>Justers</Tab>
-            <Tab sx={styleTabList}>Lawyers</Tab>
-            <Tab sx={styleTabList}>Donators</Tab>
-            <Tab sx={styleTabList}>Admin</Tab>
+            <Tab sx={styleTabList}>
+              <Image src="justerGrey.svg" />
+              Justers
+            </Tab>
+
+            <Tab sx={styleTabList}>
+              <Image src="gravelGrey.svg" />
+              Lawyers
+            </Tab>
+            <Tab sx={styleTabList}>
+              <Image src="donatorGrey.svg" />
+              Donators
+            </Tab>
+            <Tab sx={styleTabList}>
+              <Image src="adminGrey.svg" />
+              Admin
+            </Tab>
           </Flex>
         </TabList>
         <TabPanels>
